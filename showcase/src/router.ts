3ae329c327ec8f3133a3c0f4448ca1826fd12e63
Kroenigk <147001744+Kroenigk@users.noreply.github.com>
2025-07-12 02:55:45 +0000

    Fixed routing syntax

--- showcase/src/router.ts
+++ showcase/src/router.ts
@@ -1,18 +1,18 @@
 export default [
   {
     path: '/',
-    component: () => import('./home.html?raw'),
+    component: () => import('./home.html?raw').then(m => m.default),
   },
   {
     path: '/skills',
-    component: () => import('./pages/skills/skills.html?raw'),
+    component: () => import('./pages/skills/skills.html?raw').then(m => m.default),
   },
   {
     path: '/projects',
-    component: () => import('./pages/projects/projects.html?raw'),
+    component: () => import('./pages/projects/projects.html?raw').then(m => m.default),
   },
   {
     path: '/resume',
-    component: () => import('./pages/resume/resume.html?raw'),
-  },
+    component: () => import('./pages/resume/resume.html?raw').then(m => m.default),
+  }
 ];
